d54355cbd7568d80a027714affb813339f44c106
leinonenko <Kabaman1995p>
2022-01-12 15:34:58 +0200

    safeAreaView, Flexbox and Props

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView} from 'react-native';
+import GlobalStyles from './utils/GlobalStyles';
+import List from './components/List';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>First app on my phone</Text>
+    <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
+      <List />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
