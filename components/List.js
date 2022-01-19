7609c73734fcf909b442d0f93beacfed398404a5
leinonenko <Kabaman1995p>
2022-01-19 10:56:21 +0200

    navigation done(Home, Single, TabScreen)

--- components/List.js
+++ components/List.js
@@ -2,17 +2,23 @@ import React from 'react';
 import {FlatList} from 'react-native';
 import {useMedia} from '../hooks/ApiHooks';
 import ListItem from './ListItem';
+import PropTypes from 'prop-types';
+import Home from '../views/Home';
 
-const List = () => {
+const List = ({navigation}) => {
   const {mediaArray} = useMedia();
 
   return (
     <FlatList
-      data={mediaArray}
-      keyExtractor={(item) => item.file_id.toString()}
-      renderItem={({item}) => <ListItem singleMedia={item} />}
-    ></FlatList>
+  data={mediaArray}
+  keyExtractor={(item) => item.file_id.toString()}
+  renderItem={({item}) => <ListItem navigation={navigation} singleMedia={item}/>}
+  />
   );
 };
+
+List.propTypes = {
+  navigation: PropTypes.object
+}
 
 export default List;
