3a6ea014eac51d102bee07af7613ba57a0b35034
leinonenko <Kabaman1995p>
2022-01-20 15:17:29 +0200

    added doFetch fun

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,6 +1,21 @@
 import {useEffect, useState} from 'react';
 import {baseUrl} from '../utils/variables';
 
+const doFetch = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    const json = await response.json();
+    if (response.ok) {
+      return json;
+    } else {
+      const message = json.error ? `${json.message}: ${json.error}` : json.message;
+        throw new Error(json.message || response.statusText);
+      }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
   const loadMedia = async (start = 0, limit = 10) => {
@@ -44,39 +59,19 @@ const useLogin = () => {
       },
       body: JSON.stringify(userCredentials),
     };
-    try {
-      const response = await fetch(baseUrl + 'login', options);
-      const userData = await response.json();
-      if (response.ok) {
-        return userData;
-      } else {
-        throw new Error(userData.message);
-      }
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    return doFetch(baseUrl + 'login', options)
   };
-
   return {postLogin};
 };
 
 const useUser = () => {
   const getUserByToken = async (token) => {
-    try {
-      const options = {
-        method: 'GET',
-        headers: {'x-access-token': token},
-      };
-      const response = await fetch(baseUrl + 'users/user', options);
-      const userData = response.json();
-      if (response.ok) {
-        return userData;
-      } else {
-        throw new Error(userData.message);
-      }
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    const options = {
+      method: 'GET',
+      headers: {'x-access-token': token},
+    };
+    const userData = await doFetch(baseUrl + 'users/user', options);
+    return userData;
   };
 
   const postUser = async (data) => {
@@ -87,19 +82,8 @@ const useUser = () => {
       },
       body: JSON.stringify(data),
     };
-    try {
-      const response = await fetch(baseUrl + 'users', options);
-      const userData = await response.json();
-      if (response.ok) {
-        return userData;
-      } else {
-        throw new Error(userData.message + ': ' + userData.error);
-      }
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    return await doFetch(baseUrl + 'users', options);
   };
-
   return {getUserByToken, postUser};
 };
 
