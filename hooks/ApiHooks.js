8b1ed818f54f8966c00f7bd3e16c18112a0a4a1c
leinonenko <Kabaman1995p>
2022-01-20 11:47:08 +0200

    login form is done

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,8 +1,9 @@
-import {useEffect, useState} from 'react';
 import {baseUrl} from '../utils/variables';
+import {useEffect, useState} from 'react';
 
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
+
   const loadMedia = async (start = 0, limit = 10) => {
     try {
       const response = await fetch(
@@ -14,24 +15,71 @@ const useMedia = () => {
       const json = await response.json();
       const media = await Promise.all(
         json.map(async (item) => {
-          const response = await fetch(baseUrl + 'media/' + item.file_id);
+          const response = await fetch(`${baseUrl}media/${item.file_id}`);
           const mediaData = await response.json();
-          // console.log(mediaData);
+          //console.log(mediaData);
           return mediaData;
         })
       );
       setMediaArray(media);
-      console.log(mediaArray);
-    } catch (error) {
-      console.error(error);
+    } catch (e) {
+      console.error(e);
     }
   };
+
   // Call loadMedia() only once when the component is loaded
   useEffect(() => {
-    loadMedia(0);
+    loadMedia();
   }, []);
-
   return {mediaArray};
 };
 
-export {useMedia};
+const useLogin = () => {
+  const postLogin = async (userCredentials) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userCredentials),
+    };
+    try {
+      const response = await fetch(baseUrl + 'login', options);
+      const userData = await response.json();
+      if (response.ok) {
+        return userData;
+      } else {
+        throw new Error(userData.message);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+  return {postLogin};
+};
+
+const useUser = () => {
+
+  const getUserByToken = async (token) => {
+    try {
+      const options = {
+        method: 'GET',
+        headers: {'x-access-token': token},
+      };
+      const response = await fetch(baseUrl + 'users/user', options);
+      const userData = response.json();
+      if (response.ok) {
+        return userData;
+      } else {
+        throw new Error(userData.message);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+
+  return {getUserByToken};
+};
+
+
+export {useMedia, useLogin, useUser};
--- views/Login.js
+++ views/Login.js
@@ -3,16 +3,27 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useLogin, useUser} from '../hooks/ApiHooks';
+import {getUserByToken} from '../hooks/ApiHooks'
+import LoginForm from '../components/LoginForm';
 
 const Login = ({navigation}) => {
   const {setIsLoggedIn} = useContext(MainContext);
+  const {postLogin} = useLogin();
+  const {getUserByToken} = useUser();
 
   const checkToken = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
-    console.log('token value in async storage', userToken);
-    // dummy validation for user token
-    if (userToken === 'abcdef') {
+    console.log('token value from async storage', userToken);
+    if (!userToken) {
+      return;
+    }
+    try {
+      const userData = await getUserByToken(userToken);
+      console.log('checkToken', userData)
       setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -21,15 +32,21 @@ const Login = ({navigation}) => {
   }, []);
 
   const logIn = async () => {
-    console.log('Login button pressed');
-    // in real world: call api with user creds and get a token as response
-    // now we are using a "dummy" token
-    await AsyncStorage.setItem('userToken', 'abcdef');
-    setIsLoggedIn(true);
+    // hard code your username and password:
+    const data = {username: 'pavlol', password: 'asdf1234'};
+    try {
+      const userData = await postLogin(data);
+      await AsyncStorage.setItem('userToken', userData.token);
+      setIsLoggedIn(true);
+      // navigation.navigate('Tabs');
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <View style={styles.container}>
       <Text>Login</Text>
+      <LoginForm navigation={navigation}/>
       <Button title="Sign in!" onPress={logIn} />
     </View>
   );
