34dd36edf99dbd05d74b41408c69399a07ef93be
leinonenko <Kabaman1995p>
2022-01-20 13:41:47 +0200

    registration works

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,9 +1,8 @@
-import {baseUrl} from '../utils/variables';
 import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
 
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
-
   const loadMedia = async (start = 0, limit = 10) => {
     try {
       const response = await fetch(
@@ -15,27 +14,29 @@ const useMedia = () => {
       const json = await response.json();
       const media = await Promise.all(
         json.map(async (item) => {
-          const response = await fetch(`${baseUrl}media/${item.file_id}`);
+          const response = await fetch(baseUrl + 'media/' + item.file_id);
           const mediaData = await response.json();
-          //console.log(mediaData);
+          // console.log(mediaData);
           return mediaData;
         })
       );
       setMediaArray(media);
-    } catch (e) {
-      console.error(e);
+      // console.log(mediaArray);
+    } catch (error) {
+      console.error(error);
     }
   };
-
   // Call loadMedia() only once when the component is loaded
   useEffect(() => {
-    loadMedia();
+    loadMedia(0, 5);
   }, []);
+
   return {mediaArray};
 };
 
 const useLogin = () => {
   const postLogin = async (userCredentials) => {
+    // user credentials format: {username: 'someUsername', password: 'somePassword'}
     const options = {
       method: 'POST',
       headers: {
@@ -55,11 +56,11 @@ const useLogin = () => {
       throw new Error(error.message);
     }
   };
+
   return {postLogin};
 };
 
 const useUser = () => {
-
   const getUserByToken = async (token) => {
     try {
       const options = {
@@ -78,8 +79,28 @@ const useUser = () => {
     }
   };
 
-  return {getUserByToken};
-};
+  const postUser = async (data) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+    try {
+      const response = await fetch(baseUrl + 'users', options);
+      const userData = await response.json();
+      if (response.ok) {
+        return userData;
+      } else {
+        throw new Error(userData.message);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
 
+  return {getUserByToken, postUser};
+};
 
 export {useMedia, useLogin, useUser};
