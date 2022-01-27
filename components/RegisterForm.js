5406e3f1e253da800ec391a7952cb589735936c6
leinonenko <Kabaman1995p>
2022-01-27 14:02:08 +0200

    validation of username, paswd and email + paswd comfirm

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -24,6 +24,7 @@ const RegisterForm = () => {
   const onSubmit = async (data) => {
     console.log(data);
     try {
+      delete data.confirmPassword;
       const userData = await postUser(data);
       console.log('register onSubmit', userData);
     } catch (error) {
@@ -36,7 +37,8 @@ const RegisterForm = () => {
       <Controller
         control={control}
         rules={{
-          required: true,
+          required: {value: true, message: 'This is required!'},
+          minLength: {value: 3, message: 'Username has to be at least 3 char'},
           validate: async (value) => {
             try {
               const available = await checkUsername(value);
@@ -66,7 +68,11 @@ const RegisterForm = () => {
       <Controller
         control={control}
         rules={{
-          required: true,
+          required: {value: true, message: 'This is required!'},
+          /*pattern: {
+            value: /(?=.*[\p{Lu}])(?=.*[0-9]).{8,}/u,
+            message:'Min 8, Uppercase, Numbered'} */
+          minLength: {value: 5, message: 'Password has to be at least 5 char'},
         }}
         render={({field: {onChange, onBlur, value}}) => (
           <Input
@@ -76,16 +82,47 @@ const RegisterForm = () => {
             autoCapitalize="none"
             secureTextEntry={true}
             placeholder="Password"
+            errorMessage={errors.password && errors.password.message}
           />
         )}
         name="password"
       />
-      {errors.password && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+          required: {value: true, message: 'This is required!'},
+          validate: (value) => {
+            const {password} = getValues();
+            if (value === password) {
+              return true;
+            } else {
+              return 'Passwords do not match';
+            }
+          }
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            placeholder="Confirm password"
+            errorMessage={errors.confirmPassword && errors.confirmPassword.message}
+          />
+        )}
+        name="confirmPassword"
+      />
 
       <Controller
         control={control}
         rules={{
           required: true,
+          pattern: {
+            value: /^[a-z0-9_-]+(\.[a-z0-9_-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*\.[a-z]{2,}$/,  // \S+@\S+\.\S+$ as well
+            message: 'Not a email format',
+          },
         }}
         render={({field: {onChange, onBlur, value}}) => (
           <Input
@@ -94,13 +131,16 @@ const RegisterForm = () => {
             value={value}
             autoCapitalize="none"
             placeholder="Email"
+            errorMessage={errors.email && errors.email.message}
           />
         )}
         name="email"
       />
-      {errors.email && <Text>This is required.</Text>}
 
       <Controller
+        rules={{
+          minLength: {value: 3, message: 'Full name has to be at least 3 char'},
+        }}
         control={control}
         render={({field: {onChange, onBlur, value}}) => (
           <Input
@@ -109,6 +149,7 @@ const RegisterForm = () => {
             value={value}
             autoCapitalize="words"
             placeholder="Full name"
+            errorMessage={errors.full_name && errors.full_name.message}
           />
         )}
         name="full_name"
