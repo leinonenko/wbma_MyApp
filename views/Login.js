f06825cc4bd5202a0c428ce736a18290913c795e
leinonenko <Kabaman1995p>
2022-01-27 09:17:29 +0200

    Ilkka´s example

--- views/Login.js
+++ views/Login.js
@@ -1,31 +1,38 @@
 import React, {useContext, useEffect} from 'react';
-import {StyleSheet, View, Text, Button, Platform, Keyboard} from 'react-native';
+import {
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  Keyboard,
+  View,
+} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useLogin, useUser} from '../hooks/ApiHooks';
+import {useUser} from '../hooks/ApiHooks';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
-import {KeyboardAvoidingView} from 'react-native';
-import {TouchableOpacity} from 'react-native';
+import {Card, Text} from 'react-native-elements';
 
 const Login = ({navigation}) => {
-  const {setIsLoggedIn , setUser} = useContext(MainContext);
+  const {setIsLoggedIn, setUser} = useContext(MainContext);
   const {getUserByToken} = useUser();
 
   const checkToken = async () => {
     const userToken = await AsyncStorage.getItem('userToken');
-    console.log('token value from async storage', userToken);
+    console.log('token value in async storage', userToken);
     if (!userToken) {
       return;
     }
     try {
       const userData = await getUserByToken(userToken);
-      console.log('checkToken', userData)
-      setUser(userData)
+      console.log('checkToken', userData);
+      console.log('token', userToken);
+      setUser(userData);
       setIsLoggedIn(true);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -35,26 +42,46 @@ const Login = ({navigation}) => {
 
   return (
     <TouchableOpacity
-      onPress={() => Keyboard.dismiss()}
       style={{flex: 1}}
       activeOpacity={1}
+      onPress={() => Keyboard.dismiss()}
     >
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : ''} style={styles.container}>
-      <Text>Login</Text>
-      <LoginForm />
-      <RegisterForm />
-      <Button title="Sign in!" />
-    </KeyboardAvoidingView>
-      </TouchableOpacity>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        style={styles.container}
+      >
+        <View style={styles.appTitle}>
+          <Text>MyApp</Text>
+        </View>
+        <View style={styles.form}>
+          <Card>
+            <Card.Title h4>Login</Card.Title>
+            <Card.Divider />
+            <LoginForm />
+          </Card>
+          <Card>
+            <Card.Title h4>Register</Card.Title>
+            <Card.Divider />
+            <RegisterForm />
+          </Card>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 16,
+  },
+  appTitle: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    flex: 8,
   },
 });
 
